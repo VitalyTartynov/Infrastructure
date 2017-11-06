536929021777f7d85f4f0109b680ef9629c7a6b8
Vitaly.Tartynov <Vitaly.Tartynov@GMail.com>
2017-11-06 15:25:35 +0700

    Добавлен загрузчик шагов из сборки

---
+++ Infrastructure.Initializer/AssemblyStepsLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Infrastructure.Initializer
+{
+    /// <summary>
+    /// Загрузчик шагов инициализации и отката из сборки
+    /// </summary>
+    public class AssemblyStepsLoader : IStepsLoader
+    {
+        private readonly Assembly _assembly;
+
+        /// <summary>
+        /// Загрузка шагов инициализации и отката из сборки
+        /// </summary>
+        public AssemblyStepsLoader(Assembly assembly)
+        {
+            _assembly = assembly;
+        }
+
+        public ICollection<IInitializeStep> Load()
+        {
+            var types = _assembly
+                .GetTypes()
+                .Where(x => x.IsClass && typeof(IInitializeStep).IsAssignableFrom(x))
+                .ToArray();
+
+            return types
+                .Select(x => Activator.CreateInstance(x) as IInitializeStep)
+                .OrderBy(x => x.Priority)
+                .ToArray();
+        }
+    }
+}
---
+++ Infrastructure.Initializer/IStepsLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure.Initializer
+{
+    /// <summary>
+    /// Загрузчик шагов инициализации и отката
+    /// </summary>
+    public interface IStepsLoader
+    {
+        /// <summary>
+        /// Загрузка шагов инициализации и отката
+        /// </summary>
+        /// <returns>Коллекция шагов инициализации и отката</returns>
+        ICollection<IInitializeStep> Load();
+    }
+}
