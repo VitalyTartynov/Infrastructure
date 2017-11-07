4f8bc1c2bd489054c4f66c9ae18864f6c4ecd202
TartynovViA <TartynovViA@tomsknipi.ru>
2017-11-07 08:40:38 +0700

    Добавил методы для загрузки встроенных ресурсов как MemoryStream

---
+++ Infrastructure.EmbeddedResources/Infrastructure.EmbeddedResources.Tests/ResourceLoaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+
+namespace Infrastructure.EmbeddedResources.Tests
+{
+    [TestFixture]
+    internal class ResourceLoaderTests
+    {
+        [Test]
+        public void GetResourceAsStreamTest()
+        {
+            var path = @"Samples\testfile.txt";
+            var stream = ResourceLoader.GetEmbeddedResource(Assembly.GetExecutingAssembly(), path);
+
+            Assert.That(stream, Is.Not.Null);
+        }
+    }
+}
