987dc411e68f84167c3d03256bcf92658795215e
TartynovViA <TartynovViA@tomsknipi.ru>
2017-11-07 08:18:31 +0700

    Добавил еще одну библиотеку для работы с встроенными ресурсами.

---
+++ Infrastructure.EmbeddedResources/Infrastructure.EmbeddedResources.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Infrastructure.EmbeddedResources.Tests")]
+[assembly: AssemblyDescription("Tests for framework for simplify working with embedded resources")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitaly Tartynov")]
+[assembly: AssemblyProduct("Infrastructure.EmbeddedResources.Tests")]
+[assembly: AssemblyCopyright("Copyright © Vitaly.Tartynov 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15669204-16da-44ec-96a9-01e457e53546")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
