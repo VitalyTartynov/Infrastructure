eeb3be39533afea0773d0741fc04a67beac9ffd4
TartynovViA <TartynovViA@tomsknipi.ru>
2017-11-16 13:36:31 +0700

    Add main assembly for common code

---
+++ Infrastructure/Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Infrastructure")]
+[assembly: AssemblyDescription("Framework for common parts like useful extensions, attributes etc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitaly Tartynov")]
+[assembly: AssemblyProduct("Infrastructure")]
+[assembly: AssemblyCopyright("Copyright © Vitaly.Tartynov 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ceb3197f-bc25-4157-a066-b5a651870976")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
