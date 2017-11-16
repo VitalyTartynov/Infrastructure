ffe02e2b54e46213dfdf75039ae355421b8ff9ff
TartynovViA <TartynovViA@tomsknipi.ru>
2017-11-16 14:08:49 +0700

    Добавил типизированный Switch и тесты к нему

---
+++ Infrastructure/Infrastructure.Tests/TypedSwitchTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Infrastructure.Tests
+{
+    [TestFixture]
+    public class TypedSwitchTests
+    {
+        [Test]
+        public void StructTest()
+        {
+            var tswitch = new TypedSwitch<int>()
+                .Case((Func<bool, bool> x) => 35)
+                .Case((string x) => 5)
+                .Case((int x) => 200);
+
+            Assert.That(tswitch.Switch(new Func<bool, bool>(x => !x)), Is.EqualTo(35));
+            Assert.That(tswitch.Switch("50"), Is.EqualTo(5));
+            Assert.That(tswitch.Switch(25), Is.EqualTo(200));
+            Assert.That(tswitch.Switch(12.5), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ClassTest()
+        {
+            var delegate1 = new Action(() =>
+            {
+                var i = 5 + 10;
+            });
+            var delegate2 = new Action(() =>
+            {
+                var i = 20 + 30;
+            });
+
+            var tswitch = new TypedSwitch<Delegate>()
+                .Case((int x) => delegate1)
+                .Case((double y) => delegate2);
+
+            Assert.That(tswitch.Switch(2), Is.EqualTo(delegate1));
+            Assert.That(tswitch.Switch(2.5), Is.EqualTo(delegate2));
+            Assert.That(tswitch.Switch("str"), Is.EqualTo(null));
+        }
+    }
+}
---
+++ Infrastructure/Infrastructure/TypedSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// Switch по типам
+    /// </summary>
+    public class TypedSwitch<TOutputType>
+    {
+        private readonly Dictionary<Type, Func<object, TOutputType>> _matches = new Dictionary<Type, Func<object, TOutputType>>();
+
+        public TypedSwitch<TOutputType> Case<T>(Func<T, TOutputType> action)
+        {
+            _matches.Add(typeof(T), (x) => action((T)x));
+
+            return this;
+        }
+
+        public TOutputType Switch(object x)
+        {
+            var type = x.GetType();
+            if (_matches.ContainsKey(type))
+            {
+                return _matches[type](x);
+            }
+
+            return default(TOutputType);
+        }
+    }
+}
