24d028d753ee6494c567357481c1a9084afd0239
Vitaly Tartynov <Vitaly.Tartynov@GMail.com>
2018-08-12 16:31:49 +0700

    Reorganized root folders of project. Updated nUnit. Updated AssemblyInfo.

---
+++ src/Infrastructure.Initializer/Infrastructure.Initializer/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Infrastructure.Initializer")]
+[assembly: AssemblyDescription("Framework for managing and executing initialize and teardown steps in applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitaly Tartynov")]
+[assembly: AssemblyProduct("Infrastructure.Initializer")]
+[assembly: AssemblyCopyright("Copyright © Vitaly.Tartynov 2017-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e12d7def-5349-44a5-a863-c335f3390a56")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
